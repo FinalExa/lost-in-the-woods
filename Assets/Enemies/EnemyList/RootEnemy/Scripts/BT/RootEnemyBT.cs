7e577cc8662d3a5ae14f8871c27bda42b9249d08
Alex Silvestri <alexsilvestri.16@gmail.com>
2022-12-24 15:21:14 +0100

    Behavior working

--- Assets/Enemies/EnemyList/RootEnemy/Scripts/BT/RootEnemyBT.cs
+++ Assets/Enemies/EnemyList/RootEnemy/Scripts/BT/RootEnemyBT.cs
@@ -18,30 +18,39 @@ public class RootEnemyBT : EnemyBT
         {
             new TaskEnemyIsNotLocked(enemyController),
             new TaskSetRootVulnerable(rootEnemyController),
-            new Sequence (new List<Node>
+            new Selector (new List<Node>
             {
                 new TaskIsInBerserkState(enemyController, enemyWeaponSwitcher),
-                new Selector (new List<Node>
+                new Sequence(new List<Node>
                 {
-                    new TaskIsCloseToPlayer(enemyController, enemyController.enemyData.berserkDistanceFromPlayer),
-                    new TaskMoveToPlayer(enemyController, enemyController.enemyData.berserkMovementSpeed)
-                }),
-                new TaskAttackPlayer(enemyController)
+                    new Selector (new List<Node>
+                    {
+                        new TaskIsCloseToPlayer(enemyController, enemyController.enemyData.berserkDistanceFromPlayer),
+                        new TaskMoveToPlayer(enemyController, enemyController.enemyData.berserkMovementSpeed)
+                    }),
+                    new TaskAttackPlayer(enemyController)
+                })
             }),
-            new Sequence (new List<Node>
+            new Selector (new List<Node>
             {
                 new TaskIsInNormalState(enemyController, enemyWeaponSwitcher),
-                new Selector(new List<Node>
+                new Sequence(new List<Node>
                 {
-                    new TaskIsCloseToPlayer(enemyController, enemyController.enemyData.normalDistanceFromPlayer),
-                    new TaskMoveToPlayer(enemyController, enemyController.enemyData.normalMovementSpeed)
-                }),
-                new TaskAttackPlayer(enemyController)
+                    new Selector(new List<Node>
+                    {
+                        new TaskIsCloseToPlayer(enemyController, enemyController.enemyData.normalDistanceFromPlayer),
+                        new TaskMoveToPlayer(enemyController, enemyController.enemyData.normalMovementSpeed)
+                    }),
+                    new TaskAttackPlayer(enemyController)
+                })
             }),
-            new Sequence (new List<Node>
+            new Selector (new List<Node>
             {
                 new TaskIsInCalmState(enemyController, enemyWeaponSwitcher),
-                new TaskStopMovement(enemyController)
+                new Sequence(new List<Node>
+                {
+                    new TaskStopMovement(enemyController)
+                })
             })
         });
         return root;
