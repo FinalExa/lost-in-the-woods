d4a06efca4d8733e26495a4bdc27f3e4a6dc0724
Alex Silvestri <alexsilvestri.16@gmail.com>
2023-01-26 18:16:41 +0100

    Created all movable shroom plants

--- Assets/Weapons/Scripts/WeaponAttack.cs
+++ Assets/Weapons/Scripts/WeaponAttack.cs
@@ -10,7 +10,7 @@ public class WeaponAttack
     public float movementDistance;
     public bool ignoresWalls;
     public GameObject attackObject;
-    public enum WeaponAttackType { GENERIC, PLAYER, WATER, DOOR, INFESTED, WIND, LIGHT_BOMB }
+    public enum WeaponAttackType { GENERIC, PLAYER, WATER, INFESTED, LIGHT, DOOR, WIND, LIGHT_BOMB }
     public List<WeaponAttackType> weaponAttackTypes;
     [System.Serializable]
     public struct WeaponAttackHitboxSequence
