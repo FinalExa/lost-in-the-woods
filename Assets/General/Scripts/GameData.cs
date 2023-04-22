8b70e11464149e2d701a2406337d716c0002771d
Alex Silvestri <alexsilvestri.16@gmail.com>
2023-04-22 15:33:18 +0200

    Save continue

--- Assets/General/Scripts/GameData.cs
+++ Assets/General/Scripts/GameData.cs
@@ -1,10 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-[CreateAssetMenu(fileName = "GameData", menuName = "ScriptableObjects/GameData", order = 0)]
-[System.Serializable]
-public class GameData : ScriptableObject
+[Serializable]
+public class GameData
 {
-    public GameObject map;
+    public Vector3 playerPosition;
+    [Serializable]
+    public struct MapSave
+    {
+        public Zone zone;
+        public GameObject zoneStateObject;
+    }
+    public List<MapSave> mapSave;
+
+    public GameData(Vector3 playerPos, Zone[] zones)
+    {
+        SetGameData(playerPos, zones);
+    }
+
+
+    public void SetGameData(Vector3 playerPos, Zone[] zones)
+    {
+        playerPosition = playerPos;
+        if (zones.Length > 0) SetZones(zones);
+    }
+
+    private void SetZones(Zone[] zones)
+    {
+        mapSave = new List<MapSave>();
+        foreach (Zone zone in zones)
+        {
+            MapSave singleZone = new MapSave();
+            singleZone.zone = zone;
+            singleZone.zoneStateObject = zone.gameObject;
+            mapSave.Add(singleZone);
+        }
+    }
 }
--- Assets/General/Scripts/GameSave.cs
+++ Assets/General/Scripts/GameSave.cs
@@ -5,14 +5,36 @@ using System.IO;
 
 public class GameSave : MonoBehaviour
 {
-    [SerializeField] private GameData gameData;
     private string path = string.Empty;
     private string persistentPath = string.Empty;
+    public Zone[] zoneTracker;
+    public GameObject[] zoneObjectsTracker;
+    public PCController playerRef;
+    public GameData gameData;
+
+    private void Awake()
+    {
+        playerRef = FindObjectOfType<PCController>();
+        zoneTracker = FindObjectsOfType<Zone>();
+        SetZoneObjectsTracker();
+    }
+
+    private void SetZoneObjectsTracker()
+    {
+        zoneObjectsTracker = new GameObject[zoneTracker.Length];
+        for (int i = 0; i < zoneObjectsTracker.Length; i++)
+        {
+            zoneObjectsTracker[i] = zoneTracker[i].gameObject;
+        }
+    }
 
     private void Start()
     {
+        gameData = new GameData(playerRef.transform.position, zoneTracker);
         SetPaths();
+        LoadData();
     }
+
     private void SetPaths()
     {
         path = Application.dataPath + Path.AltDirectorySeparatorChar + "SaveData.json";
@@ -22,11 +44,10 @@ public class GameSave : MonoBehaviour
 
     public void SaveData()
     {
+        gameData.SetGameData(playerRef.transform.position, zoneTracker);
         string savePath = path;
 
-        print("Saving data at " + savePath);
         string json = JsonUtility.ToJson(gameData);
-        print(json);
 
         using StreamWriter writer = new StreamWriter(savePath);
         writer.Write(json);
@@ -37,6 +58,28 @@ public class GameSave : MonoBehaviour
         using StreamReader reader = new StreamReader(path);
         string json = reader.ReadToEnd();
 
-        JsonUtility.FromJsonOverwrite(json, gameData);
+        GameData dataToApply = JsonUtility.FromJson<GameData>(json);
+        if (dataToApply != null) ApplyLoadedData(dataToApply);
+    }
+
+    private void ApplyLoadedData(GameData dataToApply)
+    {
+        gameData = dataToApply;
+        List<GameData.MapSave> tempMapSave = new List<GameData.MapSave>();
+        tempMapSave = gameData.mapSave;
+        playerRef.transform.position = gameData.playerPosition;
+        for (int i = 0; i < zoneTracker.Length; i++)
+        {
+            for (int y = 0; y < tempMapSave.Count; y++)
+            {
+                if (zoneTracker[i].gameObject == tempMapSave[y].zoneStateObject)
+                {
+                    zoneTracker[i] = tempMapSave[y].zone;
+                    zoneObjectsTracker[i] = tempMapSave[y].zoneStateObject;
+                    tempMapSave.RemoveAt(y);
+                    break;
+                }
+            }
+        }
     }
 }
